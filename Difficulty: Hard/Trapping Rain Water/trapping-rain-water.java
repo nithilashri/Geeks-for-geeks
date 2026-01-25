4b85d8d02c6e2fb06ea49dd225c6de823e0ec0bf
nithilashri <100261372+nithilashri@users.noreply.github.com>
2026-01-25 15:01:56 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Trapping Rain Water/trapping-rain-water.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int maxWater(int arr[]) {
+
+        int n = arr.length;
+
+        if (n == 0) return 0;
+
+        int left = 0;
+        int right = n - 1;
+
+        int leftMax = 0;
+        int rightMax = 0;
+
+        int water = 0;
+
+        while (left < right) {
+
+            
+            if (arr[left] <= arr[right]) {
+
+                if (arr[left] >= leftMax) {
+                    leftMax = arr[left];
+                } else {
+                    water += leftMax - arr[left];
+                }
+
+                left++;
+
+            } else {
+
+                if (arr[right] >= rightMax) {
+                    rightMax = arr[right];
+                } else {
+                    water += rightMax - arr[right];
+                }
+
+                right--;
+            }
+        }
+
+        return water;
+    }
+}
