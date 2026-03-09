fd4eec148057721649a3352b25c291f4cce750e9
R P Nithila Shri <nithilashrirp2103@gmail.com>
2026-03-09 22:37:58 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Aggressive Cows/aggressive-cows.java
+++ Difficulty: Medium/Aggressive Cows/aggressive-cows.java
@@ -1,49 +1,43 @@
 import java.util.*;
-
 class Solution {
-
     public int aggressiveCows(int[] stalls, int k) {
-
+        // code here
         Arrays.sort(stalls);
-
         int n = stalls.length;
         int low = 1;
         int high = stalls[n-1] - stalls[0];
         int ans = 0;
-
-        while(low <= high){
-
-            int mid = low + (high - low) / 2;
-
-            if(canPlace(stalls, k, mid)){
+        while(low<=high){
+            int mid = low+(high-low)/2;
+            if(checkDist(stalls,k,mid)){
                 ans = mid;
-                low = mid + 1;
-            }
-            else{
-                high = mid - 1;
+                low = mid+1;
+            }else{
+                high = mid -1;
             }
         }
-
+        
         return ans;
+        
+        
     }
-
-
-    static boolean canPlace(int[] stalls, int k, int dist){
-
+    
+    static boolean checkDist(int[] stalls, int k, int dist){
+        int currentDist = stalls[0];
         int cows = 1;
-        int lastPlaced = stalls[0];
-
-        for(int i = 1; i < stalls.length; i++){
-
-            if(stalls[i] - lastPlaced >= dist){
+        for(int i=1;i<stalls.length;i++){
+            if(stalls[i]-currentDist>=dist){
                 cows++;
-                lastPlaced = stalls[i];
+                currentDist = stalls[i];
+                
+                
             }
-
-            if(cows >= k)
-                return true;
         }
-
-        return false;
+        if(cows>=k){
+            return true;
+        }else{
+            return false;
+        }
+        
     }
 }
