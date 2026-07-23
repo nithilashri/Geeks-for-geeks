61204e8f059597ba32942f0079e7c42498e30ca3
R P Nithila Shri <nithilashrirp2103@gmail.com>
2026-07-23 23:14:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Reverse a Doubly Linked List/reverse-a-doubly-linked-list.java
@@ -0,0 +1,35 @@
+/* Structure of doubly linked list node
+class Node {
+    int data;
+    Node next;
+    Node prev;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+        this.prev = null;
+    }
+}
+*/
+class Solution {
+    public Node reverse(Node head) {
+        if (head == null || head.next == null)
+            return head;
+
+        Node curr = head;
+        Node temp = null;
+
+        while (curr != null) {
+            temp = curr.prev;
+            curr.prev = curr.next;
+            curr.next = temp;
+
+            curr = curr.prev;
+        }
+
+        if (temp != null)
+            head = temp.prev;
+
+        return head;
+    }
+}
